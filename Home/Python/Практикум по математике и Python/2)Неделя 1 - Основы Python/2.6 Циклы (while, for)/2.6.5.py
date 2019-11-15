dce39483fd894f81cfceb691cc75c7ae4e91fb97
Vadzim Dziadziulia <32463123+sudoom@users.noreply.github.com>
2019-11-15 19:10:14 +0300

    . (#11)

---
+++ Home/Python/Практикум по математике и Python/2)Неделя 1 - Основы Python/2.6 Циклы (while, for)/2.6.5.py
@@ -0,0 +1,8 @@
+x = int(input())
+k = 2
+while True:
+    if x % k == 0:
+        print(k)
+        break
+    else:
+        k += 1
---
+++ Home/Python/Практикум по математике и Python/2)Неделя 1 - Основы Python/2.6 Циклы (while, for)/2.6.6.py
@@ -0,0 +1,4 @@
+x = int(input())
+x = abs(x)
+for i in range(1, x):
+    print(i**3)
